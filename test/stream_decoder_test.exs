98fb0ac8e5fa89d89f21ee0ec532a7511bde7c2e
Vandern Rodrigues <nrednav@gmail.com>
2025-08-08 01:03:32 +0100

    add boundary-case test for stream decoder

--- test/stream_decoder_test.exs
+++ test/stream_decoder_test.exs
@@ -11,4 +11,15 @@ defmodule Msgpack.StreamDecoderTest do
 
     assert result == terms
   end
+
+  test "decodes a stream where objects cross chunk boundaries" do
+    terms = [123, "elixir", true, %{"a" => 1}]
+    single_binary = Enum.map(terms, &Msgpack.encode!/1) |> Enum.join()
+    <<chunk1::binary-size(4), chunk2::binary>> = single_binary
+    input_stream = [chunk1, chunk2]
+
+    result = StreamDecoder.decode(input_stream) |> Enum.to_list()
+
+    assert result == terms
+  end
 end
