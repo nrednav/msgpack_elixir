4564c6d6bd2d56d7ffd53597b2949682a7dc81c4
Vandern Rodrigues <nrednav@gmail.com>
2025-08-09 18:20:24 +0100

    use map_join instead of map |> join

--- test/stream_decoder_test.exs
+++ test/stream_decoder_test.exs
@@ -14,7 +14,7 @@ defmodule Msgpack.StreamDecoderTest do
 
   test "decodes a stream where objects cross chunk boundaries" do
     terms = [123, "elixir", true, %{"a" => 1}]
-    single_binary = Enum.map(terms, &Msgpack.encode!/1) |> Enum.join()
+    single_binary = Enum.map_join(terms, &Msgpack.encode!/1)
     <<chunk1::binary-size(4), chunk2::binary>> = single_binary
     input_stream = [chunk1, chunk2]
 
